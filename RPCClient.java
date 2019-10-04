081b097c309db5edead3d64a756f9a9431daeb9d
OzeiasSilva <ozeiassilva2015@gmail.com>
2019-10-04 19:56:31 -0300

    Alteração da aplicação RPC-Calculadora

--- RPCClient.java
+++ RPCClient.java
@@ -1,38 +1,72 @@
-import java.io.*;
-import java.net.*;
-class RPCClient
-{
-    RPCClient()
-    {
-        try
-        {
-            InetAddress ia = InetAddress.getLocalHost();
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Calculator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ *
+ * @author 005-BCC-UESPI
+ */
+public class RPCClient {
+
+    String nome = "Ozeias";
+
+    RPCClient() {
+        try {
+            InetAddress ia = InetAddress.getByName("10.106.6.107");
             DatagramSocket ds = new DatagramSocket();
             DatagramSocket ds1 = new DatagramSocket(1300);
-            System.out.println("\nRPC Client\n");
+            System.out.println("\nRPC Client: \n");
             System.out.println("Insira a operação (ex: add, sub, mult, div) e os números que deseja calcular \n");
-            while (true)
-            {
+            while (true) {
                 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
                 String str = br.readLine();
                 byte b[] = str.getBytes();
-                DatagramPacket dp =  new DatagramPacket(b,b.length,ia,1200);
+                DatagramPacket dp = new DatagramPacket(b, b.length, ia, 1200);
                 ds.send(dp);
-                dp = new DatagramPacket(b,b.length);
+                byte b1[] = new byte[8193];
+                dp = new DatagramPacket(b1, b1.length);
                 ds1.receive(dp);
-                String s = new String(dp.getData(),0,dp.getLength());
-                System.out.println("\nResultado =" + s +"\n");
+                String s = new String(dp.getData(), 0, dp.getLength());
+                System.out.println("\nResultado" + s + "\n");
 
             }
 
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public static void main (String[] args)
-    {
+
+    public static void main(String[] args) throws IOException {
+
+        // Socket cliente = new Socket("10.106.6.107", 12345);
+        //System.out.println("O cliente se conectou ao servidor!");
         new RPCClient();
+
+//        Scanner teclado = new Scanner(System.in);
+//        PrintStream saida = new PrintStream(cliente.getOutputStream());
+//
+//        while (teclado.hasNextLine()) {
+//            saida.println(teclado.nextLine());
+//            //new RPCClient();
+//            saida.println(teclado.nextLine());
+//        }
+//        saida.close();
+//        teclado.close();
+        //cliente.close();  // lê a linha e faz algo com ela
     }
+
+    //new RPCClient();
 }
--- RPCServer.java
+++ RPCServer.java
@@ -1,86 +1,131 @@
-import java.util.*;
-import java.net.*;
-class RPCServer
-{
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Calculator;
+
+import java.io.IOException;
+import static java.lang.Math.pow;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.StringTokenizer;
+
+/**
+ *
+ * @author 005-BCC-UESPI
+ */
+public final class RPCServer {
+
+    static Socket accept() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     DatagramSocket ds;
     DatagramPacket dp;
-    String str,methodName,result;
-    float val1,val2;
-    RPCServer()
-    {
-        try
-        {
-            ds=new DatagramSocket(1200);
-            byte b[]=new byte[4096];
-            while(true)
-            {
-            dp=new DatagramPacket(b,b.length);
-            ds.receive(dp);
-            str=new String(dp.getData(),0,dp.getLength());
-            if(str.equalsIgnoreCase("q"))
-            {
-                System.exit(1);
-            }
-            else
-            {
-                StringTokenizer st = new StringTokenizer(str," ");
-                int i=0;
-                while(st.hasMoreTokens())
-                {
-                    String token=st.nextToken();
-                    methodName=token;
-                    val1 = Float.parseFloat(st.nextToken());
-                    val2 = Float.parseFloat(st.nextToken());
+    String str, methodName, result;
+    float val1, val2;
+
+    String nome = "Ozeias";
+
+    RPCServer() throws IOException {
+
+//        ServerSocket servidor = new ServerSocket(12345);
+//        System.out.println("Porta 12345 aberta!");
+//        // a continuação do servidor deve ser escrita aqui      
+//        Socket cliente = servidor.accept();
+//        System.out.println("Nova conexão com o cliente: " + cliente.getInetAddress().getHostAddress()
+//        );
+//
+//        Scanner s = new Scanner(cliente.getInputStream());
+//        
+//        
+//        while (s.hasNextLine()) {
+//        System.out.println(s.nextLine());
+        try {
+            ds = new DatagramSocket(1200);
+            byte b[] = new byte[4096];
+            while (true) {
+                dp = new DatagramPacket(b, b.length);
+                ds.receive(dp);
+                str = new String(dp.getData(), 0, dp.getLength());
+                if (str.equalsIgnoreCase("q")) {
+                    System.exit(1);
+                } else {
+                    StringTokenizer st = new StringTokenizer(str, " ");
+                    int i = 0;
+                    while (st.hasMoreTokens()) {
+                        String token = st.nextToken();
+                        methodName = token;
+                        val1 = Float.parseFloat(st.nextToken());
+                        val2 = Float.parseFloat(st.nextToken());
+                    }
                 }
+                System.out.println(str);
+                InetAddress ia = InetAddress.getLocalHost();
+                if (methodName.equalsIgnoreCase("add")) {
+                    result = "" + add(val1, val2);
+                } else if (methodName.equalsIgnoreCase("sub")) {
+                    result = "" + sub(val1, val2);
+                } else if (methodName.equalsIgnoreCase("mul")) {
+                    result = "" + mul(val1, val2);
+                } else if (methodName.equalsIgnoreCase("div")) {
+                    result = "" + div(val1, val2);
+
+                } else if (methodName.equalsIgnoreCase("mod")) {
+                    result = "" + mod(val1, val2);
+
+                } else if (methodName.equalsIgnoreCase("pow")) {
+                    result = "" + pow(val1, val2);
+                }
+                result = "[Ozeias] = " + result;
+                byte b1[] = result.getBytes();
+                DatagramSocket ds1 = new DatagramSocket();
+                DatagramPacket dp1 = new DatagramPacket(b1, b1.length, dp.getAddress(), 1300);
+                System.out.println("result : " + result + "\n");
+                System.out.println("Cliente: " + dp.getAddress());
+                ds1.send(dp1);
             }
-            System.out.println(str);
-            InetAddress ia = InetAddress.getLocalHost();
-            if(methodName.equalsIgnoreCase("add"))
-            {
-                result= "" + add(val1,val2);
-            }
-            else if(methodName.equalsIgnoreCase("sub"))
-            {
-                result= "" + sub(val1,val2);
-            }
-            else if(methodName.equalsIgnoreCase("mul"))
-            {
-                result= "" + mul(val1,val2);
-            }
-            else if(methodName.equalsIgnoreCase("div"))
-            {
-                result= "" + div(val1,val2);
-            }
-            byte b1[]=result.getBytes();
-            DatagramSocket ds1 = new DatagramSocket();
-            DatagramPacket dp1 = new DatagramPacket(b1,b1.length,InetAddress.getLocalHost(),1300);
-            System.out.println("result : "+result+"\n");
-            ds1.send(dp1);
-            }
+        } catch (IOException | NumberFormatException e) {
         }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        }
+//        }
+
+//        s.close();
+//        servidor.close();
+//        cliente.close();
     }
-    public float add(float val1, float val2)
-    {
-        return val1+val2;
+
+    public float add(float val1, float val2) {
+        return val1 + val2;
     }
-    public float sub(float val3, float val4)
-    {
-        return val3-val4;
+
+    public float sub(float val3, float val4) {
+        return val3 - val4;
     }
-    public float mul(float val3, float val4)
-    {
-        return val3*val4;
+
+    public float mul(float val3, float val4) {
+        return val3 * val4;
     }
-    public float div(float val3, float val4)
-    {
-        return val3/val4;
+
+    public float div(float val3, float val4) {
+        return val3 / val4;
     }
-    public static void main(String[] args)
-    {
+
+    public float mod(float val3, float val4) {
+        return val3 % val4;
+    }
+
+    public float power(float val3, float val4) {
+        return (float) Math.pow(val3, val4);
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Servidor rodando!");
+
         new RPCServer();
+
     }
+
 }
